389b5b4c71ef53c55defa1b26f3329627d44a3b6
NadNep <55347591+NadNep@users.noreply.github.com>
2019-09-30 15:02:02 +0300

    Add files via upload

---
+++ js/script-a6.js
@@ -0,0 +1,26 @@
+
+
+
+jQuery(function ($) {
+$('#button-start1').click(function () {
+ let val = 1 + '%';
+ $('#bar1').width(val).text(val)
+}) 
+});
+
+
+jQuery(function ($) {
+$('#button-start2').click(function () {
+ let val = 3 + '%';
+ $('#bar2').width(val).text(val)
+})
+});
+
+jQuery(function ($) {
+$('#button-start3').click(function () {
+ let val = 7 + '%';
+ $('#bar3').width(val).text(val)
+})
+});
+
+
